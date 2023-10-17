4457f30326d1da9d0d284d9aa389a38928851c64
Tim DeCillis <timdecillis@gmail.com>
2023-10-16 18:27:59 -0700

    Added assests to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ProjectPage from './projects/ProjectsPage';
 
 function App() {
   return (
@@ -21,10 +22,9 @@ function App() {
           </a>
         </header>
       </div>
-
-      <blockquote cite="Benjamin Franklin">
-        Tell me and I forget, teach me and I may remember, involve me and I learn.
-      </blockquote>
+      <div className='container'>
+        <ProjectPage />
+      </div>
     </>
   );
 }
