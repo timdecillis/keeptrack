18f20937aa4e76177426abd55121b63b26e08bc2
Tim DeCillis <timdecillis@gmail.com>
2023-10-18 22:05:16 -0700

    Created handleSave functionality

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -5,9 +5,10 @@ import ProjectForm from './ProjectForm';
 
 interface ProjectListProps {
   projects: Project[];
+  onSave: (project: Project) => void;
 }
 
-function ProjectList({ projects }: ProjectListProps) {
+function ProjectList({ projects, onSave }: ProjectListProps) {
   const [projectBeingEdited, setProjectBeingEdited] = useState({});
 
   const handleEdit = (project: Project) => {
@@ -22,7 +23,9 @@ function ProjectList({ projects }: ProjectListProps) {
       {projects.map((project) => (
         <div key={project.id} className="cols-sm">
           {project === projectBeingEdited ? (
-            <ProjectForm onCancel={cancelEditing} />
+            <ProjectForm
+            onSave={onSave}
+            onCancel={cancelEditing} />
           ) : (
             <ProjectCard project={project} onEdit={handleEdit} />
           )}
