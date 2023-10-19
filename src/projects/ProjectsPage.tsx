5e48c5930295c28fae62b97e6b72c997f7cb4ef1
Tim DeCillis <timdecillis@gmail.com>
2023-10-18 22:07:46 -0700

    Implemented save functionality

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {MOCK_PROJECTS} from './MockProjects'
 import ProjectList from './ProjectList';
+import { Project } from './Project';
 
 function ProjectPage() {
+  const saveProject = (project: Project) => {
+    console.log('Saving project:', project)
+  }
   return (
     <>
     <h1>Projects</h1>
-    <ProjectList projects={MOCK_PROJECTS}/>
+    <ProjectList
+    onSave={saveProject}
+    projects={MOCK_PROJECTS}/>
     </>
 
   );
