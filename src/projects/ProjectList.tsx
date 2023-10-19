6e6b56556fe9ad88dd527029b37e313363b1693c
Tim DeCillis <timdecillis@gmail.com>
2023-10-18 19:38:19 -0700

    Conditionally rendered the edit form

--- src/projects/ProjectList.tsx
+++ src/projects/ProjectList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Project } from './Project';
 import ProjectCard from './ProjectCard';
 import ProjectForm from './ProjectForm';
@@ -8,14 +8,23 @@ interface ProjectListProps {
 }
 
 function ProjectList({ projects }: ProjectListProps) {
-  const items = projects.map((project) => (
-    <div key={project.id} className="cols-sm">
-      <ProjectCard project={project}></ProjectCard>
-      <ProjectForm/>
-    </div>
-  ));
+  const [projectBeingEdited, setProjectBeingEdited] = useState({});
+  const handleEdit = (project: Project) => {
+    setProjectBeingEdited(project);
+  }
+
   return (
-    <div className="row">{items}</div>
+    <div className="row">
+      {projects.map((project) => (
+        <div key={project.id} className="cols-sm">
+          {project === projectBeingEdited ? (
+            <ProjectForm />
+          ) : (
+            <ProjectCard project={project} onEdit={handleEdit} />
+          )}
+        </div>
+      ))};
+    </div>
   );
 }
 
