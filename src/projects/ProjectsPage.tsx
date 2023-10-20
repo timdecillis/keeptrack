5bcd3bb945dd60acc3c0a7b1a0d1ced79f0416f0
Tim DeCillis <timdecillis@gmail.com>
2023-10-19 20:13:06 -0700

    Added save capability

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,19 +1,23 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import {MOCK_PROJECTS} from './MockProjects'
 import ProjectList from './ProjectList';
 import { Project } from './Project';
 
 function ProjectPage() {
+  const [projects, setProjects] = useState<Project[]>(MOCK_PROJECTS);
   const saveProject = (project: Project) => {
-    console.log('Saving project:', project)
+    let updatedProjects = projects.map((p: Project) => {
+      return p.id === project.id ? project : p;
+    });
+    setProjects(updatedProjects);
   }
   return (
-    <>
+    <Fragment>
     <h1>Projects</h1>
     <ProjectList
     onSave={saveProject}
-    projects={MOCK_PROJECTS}/>
-    </>
+    projects={projects}/>
+    </Fragment>
 
   );
 }
